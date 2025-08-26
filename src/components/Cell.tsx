6bb0af03a386ebea7362428e5a76f3847658ab15
kunalraj-313 <kunalraj72@gmail.com>
2025-08-26 20:01:52 +0530

    feat: grid cell render movement test

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useMemo } from "react";
 
-export default function Cell({ pos }) {
+export default function Cell({ pos, currentBlock }) {
+  const isActive = useMemo(() => {
+    return currentBlock.shape.some(
+      (blockPos) => blockPos.x === pos.x && blockPos.y === pos.y
+    );
+  }, [currentBlock.shape, pos.x, pos.y]);
   return (
-    <div className="w-[50px] h-[50px] text-xs">{`(${pos.x}, ${pos.y})`}</div>
+    <div
+      style={isActive ? { backgroundColor: currentBlock.color } : {}}
+      className="w-[50px] h-[50px] text-xs"
+    >{`(${pos.x}, ${pos.y})`}</div>
   );
 }
--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import Cell from "./Cell";
 
 function GameContainer() {
@@ -8,6 +8,17 @@ function GameContainer() {
   };
 
   const tetrisEntityTypes = [
+    {
+      name: "test",
+      color: "orange",
+      orientation: "N",
+      shape: [
+        {
+          x: 5,
+          y: 5,
+        },
+      ],
+    },
     {
       name: "I",
       color: "cyan",
@@ -106,22 +117,50 @@ function GameContainer() {
         { x: 4, y: -3 },
       ],
     },
-    {
-      name: "test",
-      color: "orange",
-      orientation: "N",
-      shape: [
-        {
-          x: 5,
-          y: 5,
-        },
-      ],
-    },
   ];
 
-  //   useEffect(()=>{
+  const [isRunning, setIsRunning] = useState<boolean>(true);
+  const [currentBlock, setCurrentBlock] = useState(tetrisEntityTypes[0]);
+  const [elapsedTime, setElapsedTime] = useState<number>(0);
+  const intervalIdRef = useRef<number | null>(null);
 
-  //   })
+  useEffect(() => {
+    if (isRunning) {
+      intervalIdRef.current = setInterval(() => {
+        setElapsedTime((prevTime) => prevTime + 1);
+        setCurrentBlock((prev) => {
+          return {
+            ...prev,
+            shape: prev.shape.map((pos) => {
+              return {
+                x: pos.x,
+                y: pos.y + 1,
+              };
+            }),
+          };
+        });
+      }, 1000);
+    }
+
+    return () => {
+      if (intervalIdRef.current !== null) {
+        clearInterval(intervalIdRef.current);
+      }
+    };
+  }, [isRunning]);
+
+  const handleStart = useCallback(() => {
+    setIsRunning(true);
+  }, []);
+
+  const handleStop = useCallback(() => {
+    setIsRunning(false);
+  }, []);
+
+  const handleReset = useCallback(() => {
+    setIsRunning(false);
+    setElapsedTime(0);
+  }, []);
 
   return (
     <div className="border border-white p-4 margin-0-auto w-fit h-fit">
@@ -130,6 +169,7 @@ function GameContainer() {
           {Array.from({ length: gridSize.x }).map((_, colIndex) => (
             <Cell
               key={colIndex}
+              currentBlock={currentBlock}
               pos={{
                 x: colIndex,
                 y: rowIndex,
