16f4ab1bf125347438cf2eb22a6a8bd6cebfe5b5
kunalraj-313 <kunalraj72@gmail.com>
2025-08-28 15:49:00 +0530

    feat: added main heading

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import GameContainer from "./components/GameContainer";
 
 function App() {
   return (
-    <div className="min-h-screen w-full flex justify-center bg-black pt-[50px]">
+    <div className="min-h-screen w-full flex flex-col items-center bg-black pt-[50px]">
+      <h1 className="text-6xl font-black text-white font-mono tracking-widest mb-8 text-shadow-lg">
+        TETRIS
+      </h1>
       <GameContainer />
     </div>
   );
